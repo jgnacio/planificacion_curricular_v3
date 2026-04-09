df1bbe5d0fbbc5f227b1d8234b67df4b607547f6
jgnacio <jgnaciogomez@gmail.com>
2026-04-08 23:47:38 -0300

    chore: move mobile_app out of repo

--- mobile_app/android/app/src/main/kotlin/uy/edu/ebi/ebi_planificacion/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package uy.edu.ebi.ebi_planificacion
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- mobile_app/lib/features/chat/domain/adk_chat_repository.dart
+++
@@ -1,8 +0,0 @@
-abstract class AdkChatRepository {
-  Future<void> createSession(String userId, String sessionId);
-  Future<String> sendMessage({
-    required String userId,
-    required String sessionId,
-    required String text,
-  });
-}
